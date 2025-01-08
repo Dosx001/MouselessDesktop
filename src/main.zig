a53aa5ef9cd25efe7950ba6cd3b872eb07977a62
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2025-01-08 15:45:06 -0800

    feat: print active window tree

--- src/main.zig
+++ src/main.zig
@@ -12,21 +12,46 @@ pub fn main() !void {
             const desktop: ?*c.AtspiAccessible = c.atspi_get_desktop(@intCast(i));
             for (0..@intCast(c.atspi_accessible_get_child_count(desktop, null))) |j| {
                 const app = c.atspi_accessible_get_child_at_index(desktop, @intCast(j), null);
+                defer c.g_object_unref(app);
                 for (0..@intCast(c.atspi_accessible_get_child_count(app, null))) |k| {
                     const win = c.atspi_accessible_get_child_at_index(app, @intCast(k), null);
-                    const state_set = c.atspi_accessible_get_state_set(win);
-                    if (c.atspi_state_set_contains(state_set, c.ATSPI_STATE_ACTIVE) == 1) {
-                        c.g_print(
-                            "%s\n",
-                            c.atspi_accessible_get_name(app, null),
-                        );
+                    defer c.g_object_unref(win);
+                    const states = c.atspi_accessible_get_state_set(win);
+                    defer c.g_object_unref(states);
+                    const name = c.atspi_accessible_get_name(app, null);
+                    defer c.g_free(name);
+                    if (c.atspi_state_set_contains(states, c.ATSPI_STATE_ACTIVE) == 1) {
+                        c.g_print("%s\n", name);
+                        print_tree(win, 2);
+                        return;
                     }
-                    c.g_object_unref(win);
-                    c.g_object_unref(state_set);
                 }
-                c.g_object_unref(app);
             }
         }
     }
     return;
+}
+
+fn print_tree(obj: ?*c.AtspiAccessible, padding: usize) void {
+    for (0..padding) |_| c.g_print(" ");
+    const name = c.atspi_accessible_get_name(obj, null);
+    defer c.g_free(name);
+    const role = c.atspi_accessible_get_role_name(obj, null);
+    defer c.g_free(role);
+    const pos = c.atspi_component_get_position(
+        c.atspi_accessible_get_component_iface(obj),
+        c.ATSPI_COORD_TYPE_SCREEN,
+        null,
+    );
+    defer c.g_free(pos);
+    c.g_print(
+        "(%s): %s, %d, %d\n",
+        name,
+        role,
+        pos.*.x,
+        pos.*.y,
+    );
+    for (0..@intCast(c.atspi_accessible_get_child_count(obj, null))) |i| {
+        print_tree(c.atspi_accessible_get_child_at_index(obj, @intCast(i), null), padding + 2);
+    }
 }
