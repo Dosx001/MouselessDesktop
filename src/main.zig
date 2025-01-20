e61ba91476c9deb27ac795ec3108d7415b8d46ec
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2025-01-19 16:08:04 -0800

    feat: turn window background transparent

--- src/main.zig
+++ src/main.zig
@@ -14,9 +14,23 @@ pub fn main() !void {
     c.gtk_init(&argc, &argv);
     const window = c.gtk_window_new(c.GTK_WINDOW_TOPLEVEL);
     const w: *c.GtkWindow = @ptrCast(window);
+    const screen = c.gtk_window_get_screen(w);
+    const visual = c.gdk_screen_get_rgba_visual(screen);
+    if (visual != null) c.gtk_widget_set_visual(window, visual);
+    const css_provider = c.gtk_css_provider_new();
+    _ = c.gtk_css_provider_load_from_data(
+        css_provider,
+        "window { background-color: transparent; }",
+        -1,
+        null,
+    );
+    c.gtk_style_context_add_provider_for_screen(
+        screen,
+        @ptrCast(css_provider),
+        c.GTK_STYLE_PROVIDER_PRIORITY_APPLICATION,
+    );
     c.gtk_window_fullscreen(w);
     c.gtk_window_present(w);
-    c.gtk_widget_set_opacity(window, 0.4);
     const app = c.app_indicator_new(
         "Mouseless Desktop",
         "application-exit",
